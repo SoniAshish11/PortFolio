da1b3481b0d2c8a922d0b0cebdf8427d41961d97
Ashish kumar <soniashishkumar164@gmail.com>
2023-08-15 23:10:54 +0530

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var footer = document.querySelector(".cards");
+document.addEventListener("DOMContentLoaded", function() {
+    const navList = document.getElementById("navList");
+    const toggleButton = document.getElementById("toggleButton");
+    const head = document.getElementsByClassName("head")
+
+    toggleButton.addEventListener("click", function() {
+        navList.classList.toggle("show");
+
+    });
+});
