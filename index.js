281850977e762b6b354c2d6d2cf2493ab0d39598
Ashish kumar <soniashishkumar164@gmail.com>
2023-08-16 21:40:19 +0530

    fourth commit

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
 });
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "soniashish8827@gmail.com",
+        Password: "As882700@",
+        To: 'soniashish8827@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "Email from portfolio",
+        Body: "Name: " + document.getElementById("name").value +
+            "<br> Email: " + document.getElementById("email").value +
+            "<br> Mobile number: " + document.getElementById("number").value +
+            "<br> Message: " + document.getElementById("message").value
+    }).then(
+        message => alert("Message sent succesfully")
+    );
+}
