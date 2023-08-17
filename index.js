b16d1c2174d64d5da3770e0a8abc4f97f21a1428
Ashish kumar <soniashishkumar164@gmail.com>
2023-08-17 17:00:45 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // toggle the navbar
     const navList = document.getElementById("navList");
     const toggleButton = document.getElementById("toggleButton");
     const head = document.getElementsByClassName("head")
+
+
 
     toggleButton.addEventListener("click", function() {
         navList.classList.toggle("show");
 
     });
-});
 
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "soniashish8827@gmail.com",
-        Password: "As882700@",
-        To: 'soniashish8827@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: "Email from portfolio",
-        Body: "Name: " + document.getElementById("name").value +
-            "<br> Email: " + document.getElementById("email").value +
-            "<br> Mobile number: " + document.getElementById("number").value +
-            "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("Message sent succesfully")
-    );
-}
+    // email submission
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const response = await fetch(form.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+                Accept: "application/json",
+            },
+        });
+
+        if (response.ok) {
+            alert("Message sent successfully!");
+            form.reset();
+        } else {
+            alert("Oops! Something went wrong.");
+        }
+    });
+
+
+
+
+
+});
